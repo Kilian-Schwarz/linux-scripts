120954bee195d00dea9bd8af20357e86fef56156
Kilian-Schwarz <Kilian@webschwarz.de>
2023-03-10 22:39:20 +0100

    Update ufw.sh

--- ufw.sh
+++ ufw.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Setzen der UFW Firewall zurück
-ufw --force reset
+# Setzen der sudo ufw Firewall zurück
+sudo ufw --force reset
 
 # Standardverhalten: Eingehende Verbindungen blockieren, ausgehende Verbindungen erlauben
-ufw default deny incoming
-ufw default allow outgoing
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
 
 # SSH-Verbindung von einem bestimmten Server erlauben
 read -p "Soll SSH nur von einem bestimmten Server erlaubt sein? [j/n]: " ssh_server
 if [[ $ssh_server == [jJ] ]]; then
     read -p "Geben Sie die IP-Adresse oder den Hostnamen des Servers an: " ssh_host
-    ufw allow from $ssh_host to any port 22 proto tcp comment "SSH von $ssh_host erlaubt"
+    sudo ufw allow from $ssh_host to any port 22 proto tcp comment "SSH von $ssh_host erlaubt"
 else
-    ufw allow ssh comment "SSH erlaubt"
+    sudo ufw allow ssh comment "SSH erlaubt"
 fi
 
 # Weitere Ports hinzufügen
@@ -30,11 +30,11 @@ while true; do
     read -p "Soll der Zugriff auf Port $port_number nur von einem bestimmten Server erlaubt sein? [j/n]: " port_server
     if [[ $port_server == [jJ] ]]; then
         read -p "Geben Sie die IP-Adresse oder den Hostnamen des Servers an: " server_name
-        ufw allow from $server_name to any port $port_number proto tcp comment "Zugriff auf Port $port_number von $server_name erlaubt"
+        sudo ufw allow from $server_name to any port $port_number proto tcp comment "Zugriff auf Port $port_number von $server_name erlaubt"
     else
-        ufw allow $port_number comment "Zugriff auf Port $port_number erlaubt"
+        sudo ufw allow $port_number comment "Zugriff auf Port $port_number erlaubt"
     fi
 done
 
 # Firewall aktivieren
-ufw enable
+sudo ufw enable
